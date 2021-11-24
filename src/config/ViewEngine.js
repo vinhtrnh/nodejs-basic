f39b6eebf55767de06f8206feada522907de5b57
Drake <trinhminh1990@gmail.com>
2021-11-24 15:25:01 +0700

    config viewengine and compiler js

---
+++ src/config/ViewEngine.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+let configViewEngine = (app) => {
+    app.set('view engine', 'ejs')
+    app.set("views", "./src/views")
+}
+
+export default configViewEngine
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
-const http = require('http');
+import express from 'express';
+import configViewEngine from './config/ViewEngine.js'
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+const app = express()
+const port = 3000
+
+configViewEngine(app)
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
